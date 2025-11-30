f3463b95891ca7ec97a2e4f6dbe022b2658f9788
Valeriy <lev.370x@gmail.com>
2025-11-30 21:26:12 +0100

    settings

--- src/common/settings.py
+++ src/common/settings.py
@@ -1,10 +1,19 @@
-from pydantic_settings import BaseSettings
+from pathlib import Path
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    root_dir    : str = "/study/ml-ops/ml-ops-prod/homework/mlops-hw01"
-    dataset_file: str = "WineQT.csv"
-    params_file: str = "params.yaml"
+    model_config = SettingsConfigDict(
+        env_file='.env',
+        env_file_encoding='utf-8',
+        extra='ignore'
+    )
+
+    # root_dir     : str = "/study/ml-ops/ml-ops-prod/homework/mlops-hw01"
+    root_dir     : str = str(Path(__file__).parent.parent.parent.resolve())
+    dataset_file: str  = "WineQT.csv"
+    params_file : str  = "params.yaml"
+    tracking_uri: str  = "http://localhost:5000"
 
 
 settings = Settings()
